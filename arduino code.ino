707dded751a0a949d2f56633feb650548aa597ab
ahmed tamer <79191875+Ahmed2793@users.noreply.github.com>
2024-08-28 16:09:55 +0300

    Update arduino code.ino

--- arduino code.ino
+++ arduino code.ino
@@ -34,22 +34,16 @@ void loop() {
     // Convert the received data to a float
     float value = data.toFloat();
 
-    if (value > 3.0 & value < 3.5) {
+    if (value > 2.9 & value < 3.1) {
       servo2.write(0);
       delay(1000);
-    } else if (value > 2.0 & value < 2.5) {
+    } else if (value > 3.9 & value < 4.1) {
       servo5.write(180);
       delay(1000);
-    } else if (value > 1.6 & value < 2.0) {
-      servo4.write(135);
+    } else if (value > 4.9 & value < 5.1) {
+      servo1.write(135);
       delay(1000);
-    } else if (value > 2.5 & value < 3.0) {
-      servo3.write(40);
-      delay(1000);
-    } else if (value > 1.45 & value < 1.6) {
-      servo1.write(40);
-      delay(1000);
-    } else if (value > 3.5) {
+    } else if (value > 5.9) {
       servo1.write(40);
       servo2.write(0);
       servo3.write(40);
